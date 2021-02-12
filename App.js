fef130ce98a102e36302395d5cf00aaebf8dfc8b
Israel Hudson <israelhudson@gmail.com>
2021-02-12 12:12:08 -0300

    adicionando estilo in-line para tamanho da imagem

--- App.js
+++ App.js
@@ -13,9 +13,21 @@ const App: () => React$Node = () => {
   return (
     <ScrollView>
       <Text>Ricardo</Text>
-      <Image source={require('./res/img/alura.jpg')} />
+      <Image
+        source={require('./res/img/alura.jpg')}
+        style={{
+          width: 300,
+          height: 300,
+        }}
+      />
       <Text>Marina</Text>
-      <Image source={require('./res/img/alura.jpg')} />
+      <Image
+        source={require('./res/img/alura.jpg')}
+        style={{
+          width: 300,
+          height: 300,
+        }}
+      />
     </ScrollView>
   );
 };
